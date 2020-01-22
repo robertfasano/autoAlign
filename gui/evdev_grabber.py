1750cfc7d88c8f4d54e21639b8cc81fd20a70672
Robbie Fasano <robert.j.fasano@colorado.edu>
2020-01-22 14:46:41 +0000

    Initial commit: touchscreen event grabber using evdev

---
+++ gui/evdev_grabber.py
@@ -0,0 +1,36 @@
+import evdev
+
+def parse_event(dev):
+    pos = [None, None]
+    sync_counter = 0
+    for event in dev.read_loop():
+        #print(evdev.categorize(event))
+        if event.code == 0 and event.type == 0:
+            sync_counter += 1
+        if sync_counter >= 2 and None not in pos:
+            break
+        if event.type == evdev.ecodes.EV_ABS:
+            if event.code == 0:
+                pos[0] = event.value
+            elif event.code == 1:
+                pos[1] = event.value
+       
+    return pos
+
+def to_pygame_coords(pos, height):
+    ''' Converts an [X, Y] position from evdev coordinates to pygame coordinates.
+        For a 320x240 display, Evdev coordinates are specified as:
+            * Top left: (240, 0)
+            * Bottom left: (0, 0)
+            * Top right: (240, 320)
+            * Bottom right): (0, 320)
+        Pygame coordinates are conventional computer graphics coordinates with 
+        the origin at the upper left corner.
+    '''
+    return [pos[1], height-pos[0]]
+
+if __name__ == '__main__':
+    dev = evdev.InputDevice('/dev/input/event0')
+    while True:
+        pos = parse_event(dev)
+        print(to_pygame_coords(pos, 240))
